d96dd8b1d8bee52fd7c78435a7611047da88e4a2
Danielle Despatie <dani.despatie@gmail.com>
2025-01-17 15:40:01 -0500

    Basic Structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyles from './GlobalStyles';
+import React from 'react';
+
+// Component Imports
+
+
+const App = () => {
+    return (
+        <p>Hello world</p>
+    )
+}
+
+export default App;
